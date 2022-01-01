5248b69fafa3a4da4d4080a73e23d286623d68e9
Id Brahim Hakim <la208531@student.helha.be>
2022-01-01 04:57:06 +0100

    commencement

--- vue/connection.php
+++ vue/connection.php
@@ -2,7 +2,6 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
     <title>Connection</title>
@@ -15,12 +14,12 @@
     <form action="connection" method="post" action="../controleur/formulaire.php">
         <table class="centrer">
             <tr>
-                <td> <label for="pseudo">Pseudo :</label></td>
-                <td> <input type="text" id="pseudo" name="pseudo" required></td>
+                <td> <label for="emailPsw">Pseudo ou e-mail :</label></td>
+                <td> <input type="text" id="emailPsw" name="emailPsw" required></td>
             </tr>
             <tr>
-                <td> <label for="email">e-mail :</label></td>
-                <td> <input type="email" id="email" name="email" required></td>
+                <td> <label for="pswConn">e-mail :</label></td>
+                <td> <input type="password" id="pswConn" name="pswConn" required></td>
             </tr>
             <tr>
                 <td colspan="2" class="titre"> <input type="submit" value="Connection"></td>
