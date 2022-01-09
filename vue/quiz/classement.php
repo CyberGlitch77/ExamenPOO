8f9bb62c44eabf182103d1e148b49ffcee5db536
Hakim Id Brahim <la208531@student.helha.be>
2022-01-09 17:04:01 +0100

    final

--- vue/quiz/classement.php
+++ vue/quiz/classement.php
@@ -22,30 +22,20 @@ include_once "../../controleur/quiz/classementQuiz.php";
         <h1 class="titre">Le classement du quiz</h1>
         <hr>
         <?php
-        if (isset($_SESSION["pseudo"]) || isset($_SESSION["emailPseudo"])) {
-            $resultat = $classement->classement();
-            $i = 1;
-            if (!empty($resultat)) {
-                foreach ($resultat as $row) { ?>
-                    <table class="centrer" >
-                        <tr>
-                            <td><h2 class="titre"><?php echo $i; ?>. <?php echo $row["pseudo"] ?> : moyenne de <?php echo $row["scorePond"]; ?> point(s), <?php echo $row["scoreTotal"]; ?> point(s) en <?php echo $row["essais"]?> partie(s).</h2></td><br>
-                        </tr>
-                    </table>
-            <?php
-                    $i++;
-                }
+        $resultat = $classement->classement();
+        $i = 1;
+        if (!empty($resultat)) {
+            foreach ($resultat as $row) { ?>
+                <table class="centrer">
+                    <tr>
+                        <td>
+                            <h2 class="titre"><?php echo $i; ?>. <?php echo $row["pseudo"] ?> : moyenne de <?php echo $row["scorePond"]; ?> point(s), <?php echo $row["scoreTotal"]; ?> point(s) en <?php echo $row["essais"] ?> partie(s).</h2>
+                        </td><br>
+                    </tr>
+                </table>
+        <?php
+                $i++;
             }
-            ?>
-        <?php
-        } else {
-        ?>
-            <h2 class="titre">Pour pouvoir avoir accès au classement vous devez vous connecter!!!</h2>
-            <div class="centrer">
-                <button id="inscription" class="leftbutton">Inscription</button>
-                <button id="connection" class="rightbutton">Connection</button>
-            </div>
-        <?php
         }
         ?>
     </div>
